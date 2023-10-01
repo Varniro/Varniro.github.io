591035e1269c0504353ec1a2ee17c5e027c1fc53
BuildTools <dhruvmodi428@gmail.com>
2023-10-01 11:24:33 +0530

    updated key for animegen

--- AnimeQuotes/main.js
+++ AnimeQuotes/main.js
@@ -1,5 +1,5 @@
 const url = "https://waifu.it/api/quote";
-const key = "MzE5NDYxNjc2Mjg4MDQ5MTU0.MTY5MDgyMTA1OQ--.cf25fa6769e6"
+const key = "MzE5NDYxNjc2Mjg4MDQ5MTU0.MTY5NjEzODc0OA--.c2e907f279d"
 axios.get(url,{headers:{
     Authorization: key
 }}).then((res) =>{
